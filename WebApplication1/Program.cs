e3312bf3ee42d78e8f0c7fb9f2feef92073dde6d
Yixin <zhaozicen951230@gmail.com>
2024-06-20 10:32:54 +0200

    add Swagger for vs code test

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,13 +1,42 @@
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Models;
+using NSwag.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+//Configure Swagger middleware for vs code (if you are using visual studio, you don't need this step)
+//Enable the API Explorer, which is a service that provides metadara about the HTTP API. the API Explorer is used by
+//Swagger to generate the Swagger document.
+builder.Services.AddEndpointsApiExplorer();
+
+//Add the Swagger OpenApi Document generator to the application services and configures it to provide more infomation about the API
+//such as its title and version.
+builder.Services.AddOpenApiDocument(config => {
+    config.DocumentName = "WebApplication1";
+    config.Title = "TodoAPI v1";
+    config.Version = "v1";
+});
+
 var app = builder.Build();
 
-//The sample app implements several GET endpoints by calling MapGet:
+//Enable the Swagger middleware for serving the generated JSON document and the Swagger UI.
+//Swagger is only enabled in a development environment.
+//Enabling Swagger in a production environment could expose potentially sensitive details about the API's structure and implementation
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "TodoAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
 
+//The sample app implements several GET endpoints by calling MapGet:
 //Get all to-do items
 app.MapGet("/todoitems", async (TodoDb db) =>
     await db.Todos.ToListAsync());
